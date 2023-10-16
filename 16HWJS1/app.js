b47787a7e90d2a74846827da2e73cdd88617d71f
SlavaSid <139711382+SlavaSid@users.noreply.github.com>
2023-10-16 19:36:09 +0300

    Add files via upload

--- 16HWJS1/app.js
+++ 16HWJS1/app.js
@@ -1,33 +1,33 @@
-const blockTop = document.querySelector('.top');
-const blockBottom = document.querySelector('.bottom')
-const topTown = blockTop.querySelector('.top-town')
-const topTime = blockTop.querySelector('.top-time')
-const divTopCenter = blockTop.querySelector('.top__center')
-const topBottomText = blockTop.querySelector('.top__bottom-text')
-const topBottomSpeed = blockTop.querySelector('.top__bottom-speed')
+const divBase = document.querySelector('.base');
+const divBottom = document.querySelector('.bottom')
+const baseTown = divBase.querySelector('.base-town')
+const baseTime = divBase.querySelector('.base-time')
+const divTopCenter = divBase.querySelector('.base__center')
+const baseBottomText = divBase.querySelector('.base__bottom-text')
+const baseBottomSpeed = divBase.querySelector('.base__bottom-speed')
 
 
 function renderMain(town, img, event, temp, speed) {
     
-    topTown.textContent = town
-    topTime.textContent = new Date().toLocaleTimeString();
+    baseTown.textContent = town
+    baseTime.textContent = new Date().toLocaleTimeString();
     setInterval(() => {
         const watch = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'});
-        topTime.textContent = watch;
+        baseTime.textContent = watch;
     }, 1000);
     divTopCenter.insertAdjacentHTML('beforeend', `
     <img src="./images/${img}.png" alt="icon" >
     <p class="top__center-event">${event}</p>
     <p class="top__center-temp">${Math.round(temp)} °C</p>
     `);
-    topBottomText.textContent = 'Speed'
-    topBottomSpeed.textContent = `${speed.toFixed(1)} m/s`
+    baseBottomText.textContent = 'Speed'
+    baseBottomSpeed.textContent = `${speed.toFixed(1)} m/s`
 }
 
 function renderRow(date, img, temp) {
     const data = date.slice(0, 10)
     const time = date.slice(11)
-    blockBottom.insertAdjacentHTML('beforeend', `
+    divBottom.insertAdjacentHTML('beforeend', `
     <div class="bottom__item">
         <div>
             <p>${data}</p>
@@ -40,8 +40,8 @@ function renderRow(date, img, temp) {
 }
     
 navigator.geolocation.getCurrentPosition(
-    function (position) {
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=cce47a5ecf3da9ec417a11a3844e2f70&units=metric`)
+    function (instal) {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${instal.coords.latitude}&lon=${instal.coords.longitude}&appid=cce47a5ecf3da9ec417a11a3844e2f70&units=metric`)
         .then(response => {
             if(!response.ok){
              console.error('не верное значение')
